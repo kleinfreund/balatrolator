ef723a5d1cea8c3c938b3d6bce4c63f04864604b
kleinfreund <5774638+kleinfreund@users.noreply.github.com>
2025-01-12 09:34:36 +0100

    chore: remove popstate trigger

--- src/ui/main.ts
+++ src/ui/main.ts
@@ -4,13 +4,6 @@ import { UiState } from './UiState.js'
 
 const uiState = new UiState()
 
-// Populate the UI when the user navigates back/forth through the browser history.
-window.addEventListener('popstate', fetchStateAndPopulateUi)
-
-fetchStateAndPopulateUi()
-
-function fetchStateAndPopulateUi () {
-	// Read the state from the URL first, then read it from web storage, and finally, fall back to the default/initial state.
-	const state = readStateFromUrl() ?? uiState.getAutoSaveState() ?? getState({})
-	uiState.populateUiWithState(state)
-}
+// Read the state from the URL first, then read it from web storage, and finally, fall back to the default/initial state.
+const state = readStateFromUrl() ?? uiState.getAutoSaveState() ?? getState({})
+uiState.populateUiWithState(state)
