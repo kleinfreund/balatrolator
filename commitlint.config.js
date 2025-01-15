03b41aabf84df3a0b0d459881df3d4ee1c2419c9
kleinfreund <5774638+kleinfreund@users.noreply.github.com>
2025-01-15 22:01:42 +0100

    chore: simplify config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,8 @@
-/** @type {import('@commitlint/types').UserConfig} */ const config = {
+/** @type {import('@commitlint/types').UserConfig} */
+export default {
 	extends: ['@commitlint/config-conventional'],
 	rules: {
 		'body-max-line-length': [0, 'always', Infinity],
 		'footer-max-line-length': [0, 'always', Infinity],
 	},
 }
-
-export default config
