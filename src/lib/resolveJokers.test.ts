dde90b9d334e8407a3246d102287efa5d27eae3d
kleinfreund <5774638+kleinfreund@users.noreply.github.com>
2026-03-05 13:45:34 +0100

    fix(core): blueprint in right-most position leading to crash

--- src/lib/resolveJokers.test.ts
+++ src/lib/resolveJokers.test.ts
@@ -3,127 +3,148 @@ import { describe, expect, test } from 'vitest'
 import { resolveJoker } from './resolveJokers.ts'
 
 describe('resolveJoker', () => {
-	test.each([
+	test.each<[Parameters<typeof resolveJoker>, ReturnType<typeof resolveJoker>]>([
 		[
 			[
+				[
+					{ index: 0, name: 'Blueprint' },
+					{ index: 1, name: 'Sock and Buskin' },
+				],
 				{ index: 0, name: 'Blueprint' },
-				{ index: 1, name: 'Sock and Buskin' },
 			],
-			{ index: 0, name: 'Blueprint' },
 			{ index: 1, name: 'Sock and Buskin' },
 		],
 		[
 			[
+				[
+					{ index: 0, name: 'Sock and Buskin' },
+					{ index: 1, name: 'Blueprint' },
+					{ index: 2, name: 'Brainstorm' },
+					{ index: 3, name: 'Brainstorm' },
+					{ index: 4, name: 'Brainstorm' },
+				],
 				{ index: 0, name: 'Sock and Buskin' },
-				{ index: 1, name: 'Blueprint' },
-				{ index: 2, name: 'Brainstorm' },
-				{ index: 3, name: 'Brainstorm' },
-				{ index: 4, name: 'Brainstorm' },
 			],
-			{ index: 0, name: 'Sock and Buskin' },
 			{ index: 0, name: 'Sock and Buskin' },
 		],
 		[
 			[
-				{ index: 0, name: 'Sock and Buskin' },
+				[
+					{ index: 0, name: 'Sock and Buskin' },
+					{ index: 1, name: 'Blueprint' },
+					{ index: 2, name: 'Brainstorm' },
+					{ index: 3, name: 'Brainstorm' },
+					{ index: 4, name: 'Brainstorm' },
+				],
 				{ index: 1, name: 'Blueprint' },
-				{ index: 2, name: 'Brainstorm' },
-				{ index: 3, name: 'Brainstorm' },
-				{ index: 4, name: 'Brainstorm' },
 			],
-			{ index: 1, name: 'Blueprint' },
 			{ index: 0, name: 'Sock and Buskin' },
 		],
 		[
 			[
-				{ index: 0, name: 'Sock and Buskin' },
-				{ index: 1, name: 'Blueprint' },
-				{ index: 2, name: 'Brainstorm' },
+				[
+					{ index: 0, name: 'Sock and Buskin' },
+					{ index: 1, name: 'Blueprint' },
+					{ index: 2, name: 'Brainstorm' },
+					{ index: 3, name: 'Brainstorm' },
+					{ index: 4, name: 'Brainstorm' },
+				],
 				{ index: 3, name: 'Brainstorm' },
-				{ index: 4, name: 'Brainstorm' },
 			],
-			{ index: 3, name: 'Brainstorm' },
 			{ index: 0, name: 'Sock and Buskin' },
 		],
 		[
 			[
+				[
+					{ index: 0, name: 'Blueprint' },
+					{ index: 1, name: 'Brainstorm' },
+				],
 				{ index: 0, name: 'Blueprint' },
+			],
+			undefined,
+		],
+		[
+			[
+				[
+					{ index: 0, name: 'Blueprint' },
+					{ index: 1, name: 'Brainstorm' },
+				],
 				{ index: 1, name: 'Brainstorm' },
 			],
-			{ index: 0, name: 'Blueprint' },
 			undefined,
 		],
 		[
 			[
+				[
+					{ index: 0, name: 'Blueprint' },
+					{ index: 1, name: 'Blueprint' },
+					{ index: 2, name: 'Brainstorm' },
+				],
 				{ index: 0, name: 'Blueprint' },
-				{ index: 1, name: 'Brainstorm' },
 			],
 			undefined,
-			undefined,
 		],
 		[
 			[
-				{ index: 0, name: 'Blueprint' },
-				{ index: 1, name: 'Brainstorm' },
-			],
-			{ index: 1, name: 'Brainstorm' },
-			undefined,
-		],
-		[
-			[
-				{ index: 0, name: 'Blueprint' },
+				[
+					{ index: 0, name: 'Baron' },
+					{ index: 1, name: 'Blueprint' },
+					{ index: 2, name: 'Blueprint' },
+					{ index: 3, name: 'Blueprint' },
+					{ index: 4, name: 'Mime' },
+					{ index: 5, name: 'Blueprint' },
+					{ index: 6, name: 'Brainstorm' },
+					{ index: 7, name: 'Brainstorm' },
+					{ index: 8, name: 'Brainstorm' },
+				],
 				{ index: 1, name: 'Blueprint' },
-				{ index: 2, name: 'Brainstorm' },
 			],
-			{ index: 0, name: 'Blueprint' },
-			undefined,
-		],
-		[
-			[
-				{ index: 0, name: 'Baron' },
-				{ index: 1, name: 'Blueprint' },
-				{ index: 2, name: 'Blueprint' },
-				{ index: 3, name: 'Blueprint' },
-				{ index: 4, name: 'Mime' },
-				{ index: 5, name: 'Blueprint' },
-				{ index: 6, name: 'Brainstorm' },
-				{ index: 7, name: 'Brainstorm' },
-				{ index: 8, name: 'Brainstorm' },
-			],
-			{ index: 1, name: 'Blueprint' },
 			{ index: 4, name: 'Mime' },
 		],
 		[
 			[
-				{ index: 0, name: 'Baron' },
-				{ index: 1, name: 'Blueprint' },
-				{ index: 2, name: 'Blueprint' },
-				{ index: 3, name: 'Blueprint' },
-				{ index: 4, name: 'Mime' },
+				[
+					{ index: 0, name: 'Baron' },
+					{ index: 1, name: 'Blueprint' },
+					{ index: 2, name: 'Blueprint' },
+					{ index: 3, name: 'Blueprint' },
+					{ index: 4, name: 'Mime' },
+					{ index: 5, name: 'Blueprint' },
+					{ index: 6, name: 'Brainstorm' },
+					{ index: 7, name: 'Brainstorm' },
+					{ index: 8, name: 'Brainstorm' },
+				],
 				{ index: 5, name: 'Blueprint' },
-				{ index: 6, name: 'Brainstorm' },
-				{ index: 7, name: 'Brainstorm' },
-				{ index: 8, name: 'Brainstorm' },
 			],
-			{ index: 5, name: 'Blueprint' },
 			{ index: 0, name: 'Baron' },
 		],
 		[
 			[
-				{ index: 0, name: 'Baron' },
-				{ index: 1, name: 'Blueprint' },
-				{ index: 2, name: 'Blueprint' },
-				{ index: 3, name: 'Blueprint' },
-				{ index: 4, name: 'Mime' },
-				{ index: 5, name: 'Blueprint' },
-				{ index: 6, name: 'Brainstorm' },
+				[
+					{ index: 0, name: 'Baron' },
+					{ index: 1, name: 'Blueprint' },
+					{ index: 2, name: 'Blueprint' },
+					{ index: 3, name: 'Blueprint' },
+					{ index: 4, name: 'Mime' },
+					{ index: 5, name: 'Blueprint' },
+					{ index: 6, name: 'Brainstorm' },
+					{ index: 7, name: 'Brainstorm' },
+					{ index: 8, name: 'Brainstorm' },
+				],
 				{ index: 7, name: 'Brainstorm' },
-				{ index: 8, name: 'Brainstorm' },
 			],
-			{ index: 7, name: 'Brainstorm' },
 			{ index: 0, name: 'Baron' },
 		],
-	])('works', (jokers, target, expectedJoker) => {
-		expect(resolveJoker(jokers, target)).toEqual(expectedJoker)
+		[
+			[
+				[
+					{ index: 0, name: 'Blueprint' },
+				],
+				{ index: 0, name: 'Blueprint' },
+			],
+			undefined,
+		],
+	])('works', (parameters, expected) => {
+		expect(resolveJoker(...parameters)).toEqual(expected)
 	})
 })
--- src/lib/resolveJokers.ts
+++ src/lib/resolveJokers.ts
@@ -1,31 +1,27 @@
+import { JokerName } from '#lib/types.ts'
+
 /**
  * Resolves a joker to its “Copy” target if applicable
  *
  * **Example**: a “Blueprint” copying “Brainstorm” copying “Sock and Buskin” is resolved as “Sock and Buskin”.
  */
-export function resolveJoker<Joker extends { name: string, index: number }> (jokers: Joker[], target: Joker | undefined, visitedIndexes = new Set<number>()): Joker | undefined {
-	if (!target) {
+export function resolveJoker<Joker extends { name: JokerName, index: number }> (jokers: Joker[], target: Joker, visitedIndexes = new Set<number>()): Joker | undefined {
+	// Immediately resolve non-copy joker
+	if (target.name !== 'Blueprint' && target.name !== 'Brainstorm') {
+		return target
+	}
+
+	const copyTarget = jokers.at(target.name === 'Blueprint' ? target.index + 1 : 0)
+	if (copyTarget === undefined) {
 		return undefined
 	}
 
-	let copyTarget: Joker | undefined
-	if (target.name === 'Blueprint') {
-		copyTarget = jokers[target.index + 1]
-	} else if (target.name === 'Brainstorm') {
-		copyTarget = jokers[0]
+	// Cycle detection! Resolves to no joker if the copy jokers are in an infinite loop (e.g. Blueprint first, Brainstorm second)
+	if (visitedIndexes.has(copyTarget.index)) {
+		return undefined
 	}
 
-	if (copyTarget) {
-		// Cycle detection! Resolves to no joker if the copy jokers are in an infinite loop (e.g. Blueprint first, Brainstorm second)
-		if (visitedIndexes.has(copyTarget.index)) {
-			return undefined
-		}
+	visitedIndexes.add(copyTarget.index)
 
-		visitedIndexes.add(copyTarget.index)
-
-		return resolveJoker(jokers, copyTarget, visitedIndexes)
-	}
-
-
-	return target
+	return resolveJoker(jokers, copyTarget, visitedIndexes)
 }
