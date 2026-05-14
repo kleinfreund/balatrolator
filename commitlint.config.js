17bb92617be4121058dbce1bb13eeff25c7d17f0
kleinfreund <5774638+kleinfreund@users.noreply.github.com>
2026-05-14 16:06:52 +0200

    chore: update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,7 @@
 export default {
 	extends: ['@commitlint/config-conventional'],
 	rules: {
+		'header-max-length': [0, 'always', Infinity],
 		'body-max-line-length': [0, 'always', Infinity],
 		'footer-max-line-length': [0, 'always', Infinity],
 	},
