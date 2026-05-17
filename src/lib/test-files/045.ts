400ea366bb2fb2d4a323ef5165dd6059111f2623
kleinfreund <5774638+kleinfreund@users.noreply.github.com>
2026-05-17 12:40:03 +0200

    chore(core): update test case to match report

--- src/lib/test-files/045.ts
+++ src/lib/test-files/045.ts
@@ -4,6 +4,15 @@ export default (message: string): TestCase => {
 	return {
 		message,
 		initialState: {
+			observatory: {
+				Flush: 30,
+			},
+			handLevels: {
+				Flush: {
+					level: 25,
+					plays: 36,
+				},
+			},
 			cards: [
 				{ played: true, rank: 'Queen', suit: 'Spades', enhancement: 'Mult', seal: 'Red' },
 				{ played: true, rank: 'Queen', suit: 'Spades', enhancement: 'Mult' },
@@ -17,7 +26,7 @@ export default (message: string): TestCase => {
 				{ name: 'Blueprint' },
 				{ name: 'Yorick', timesMultiplier: 23 },
 				{ name: 'Triboulet' },
-				{ name: 'Perkeo' },
+				{ name: 'Perkeo', edition: 'Polychrome' },
 			],
 		},
 		expected: {
@@ -30,9 +39,9 @@ export default (message: string): TestCase => {
 				{ rank: '10', suit: 'Spades', enhancement: 'Mult' },
 			],
 			results: [
-				{ chips: '215', multiplier: '1210881', score: '260339415', formattedScore: '260,339,415', luck: 'none' },
-				{ chips: '215', multiplier: '1210881', score: '260339415', formattedScore: '260,339,415', luck: 'average' },
-				{ chips: '215', multiplier: '1210881', score: '260339415', formattedScore: '260,339,415', luck: 'all' },
+				{ chips: '575', multiplier: '1283118607572.40836674055', score: '737793199354134', formattedScore: '7.378e14', luck: 'none' },
+				{ chips: '575', multiplier: '1283118607572.40836674055', score: '737793199354134', formattedScore: '7.378e14', luck: 'average' },
+				{ chips: '575', multiplier: '1283118607572.40836674055', score: '737793199354134', formattedScore: '7.378e14', luck: 'all' },
 			],
 		},
 	}
